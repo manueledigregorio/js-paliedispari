bc823dc0438166a18ac24ecc91f14634ab9ebf37
manueledigregorio <dig.manuele@gmail.com>
2023-09-15 16:41:47 +0200

    [add] js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//1. inizializzare una variabile con il promt 'inserire una parola'
+let inserisciParola = prompt('inserisci una parola')
+
+const myarray = inserisciParola.split (' ')
+
+console.log(myarray)
+//1. creare una funzione  che paragona la lettura  per rendere dinamico 
+//1. creare un ciclo per verificare se la parola e palindroma 
