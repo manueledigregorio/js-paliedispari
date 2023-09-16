169c6facdc1c254723d05f3838a10d86708b4429
manueledigregorio <dig.manuele@gmail.com>
2023-09-16 18:12:39 +0200

    [add] risultato

--- js/script2.js
+++ js/script2.js
@@ -1,16 +1,45 @@
 const pariDispari = prompt('inserisci pari o dipspari!');
 const numero = prompt('inserisci numero da 1 a 5');
 const randomPC = generatoreRandom();
+const somma = numero + randomPC;
+
+
+const risultato = verifica(somma);
+let messaggio;
+
+if(pariDispari === risultato ){
+
+  messaggio = ' hai vinto';
+
+}else {
+
+
+  messaggio = 'hai perso';
+}
+
+document.getElementById('output').innerHTML = messaggio
 
 
 
 
 
 
+// funzione prende la somma e verifica se è pari o dispari
+function verifica(somma){
 
+  let messaggio ;
+  if(somma % 2 === 0 ){
 
+  messaggio = 'pari'
 
+  }else{
 
+    messaggio = 'dispari';
+  }
+
+ return messaggio;
+}
+// funzione per generare numero random PC 
 function generatoreRandom(){
 
   const numeroRandom = Math.ceil(Math.random() * 5 );
@@ -19,3 +48,4 @@ function generatoreRandom(){
 
 
 }
+
